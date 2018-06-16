084eb6836d8ace19a9c6ab9b6681f772befd49d7
Stefan <stefan@neuland.io>
2018-06-16 20:09:26 +0200

    fix password reset workflow

--- radioco/urls.py
+++ radioco/urls.py
@@ -34,8 +34,14 @@ urlpatterns = [
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 
     url(r'^', include(admin.site.urls)),
-    url(r'^password_reset/$', auth_views.password_reset, name='admin_password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.password_reset,
+        name='admin_password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^password_reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password_reset/complete/$', auth_views.password_reset_complete,
+        name='password_reset_complete'),
     url(r'^api/2/', include('radioco.api.urls', namespace="api"))]
 
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
